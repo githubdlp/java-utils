b867f0dd1f58c2d05b4e5c6a00613e3ff6736b4f
dlp <maildlp@gmail.com>
2015-07-28 17:32:16 +0530

    added random string generator util class

---
+++ src/main/java/com/dlp/social/java_utils/RandomStringGenerator.java
@@ -0,0 +1,32 @@
+package com.dlp.social.java_utils;
+
+import java.util.Random;
+
+public class RandomStringGenerator {
+
+	private static Random random = new Random();
+
+	private char[] characterSet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+			'j', 'k', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+			'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'M',
+			'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '2',
+			'3', '4', '5', '6', '7', '8', '9' };
+
+	public RandomStringGenerator(char[] characterSet) {
+		if (characterSet != null && characterSet.length > 2) {
+			this.characterSet = characterSet;
+		}
+	}
+
+	public String getRandomString(int length) {
+		if (length < 1) {
+			throw new IllegalArgumentException("Ridiculous password length "
+					+ length);
+		}
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < length; i++) {
+			sb.append(characterSet[random.nextInt(characterSet.length)]);
+		}
+		return sb.toString();
+	}
+}
